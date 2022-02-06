b2d56ae0ae58ae490e1e4ce29a34e9945278ca49
Nikita Kudenikov <KudenikovNS@yandex.ru>
2022-02-06 11:32:04 +0300

    Исправлены ошибки_Р_1

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,34 @@
-
 // Переменные для попапа
-let popup = document.querySelector('.popup');
-let openPopupButton = document.querySelector('.profile__button-edit');
-let closePopupButton = document.querySelector('.popup__button-close');
+const popup = document.querySelector(".popup");
+const openPopupButton = document.querySelector(".profile__button-edit");
+const closePopupButton = document.querySelector(".popup__button-close");
 
-// Переменные для формы 
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-profession');
+// Переменные для формы
+const formElement = document.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__input-name");
+const jobInput = document.querySelector(".popup__input-profession");
 
 // Еще переменные
-let profileName = document.querySelector('.profile__title');
-let profileJob = document.querySelector('.profile__subtitle');
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__subtitle");
 
-function openPopup () {
-  popup.classList.add('popup__opened');
+function openPopup() {
+  popup.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-function closePopup () {
-  popup.classList.remove('popup__opened');
+function closePopup() {
+  popup.classList.remove("popup_opened");
 }
 
-function formSubmitHandler (evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closePopup ()
+  closePopup();
 }
 
-openPopupButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+openPopupButton.addEventListener("click", openPopup);
+closePopupButton.addEventListener("click", closePopup);
+formElement.addEventListener("submit", handleProfileFormSubmit);
